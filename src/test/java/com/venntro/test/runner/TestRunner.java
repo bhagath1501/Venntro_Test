bf613c321289cde2863c5321d2966c7ff7ffce5a
bhagath1501 <bhagath1501@gmail.com>
2017-03-13 11:12:09 +0000

    Introduced Runner class

--- src/test/java/com/venntro/test/runner/TestRunner.java
+++ src/test/java/com/venntro/test/runner/TestRunner.java
@@ -1,5 +1,16 @@
 package com.venntro.test.runner;
 
-public class TestRunner {
+import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/tes/resources",
+		glue = {"com/venntro/test/stepdefinitions"},
+		plugin = {"html:target/cucumber-hrml-reports",
+				  "json:target/cucumber.json"})
+public class TestRunner {
+	
 }
